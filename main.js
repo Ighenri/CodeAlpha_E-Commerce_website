446ed621d208350f1ee11c707e905f2c5d3a47f9
Henry <igwehenry740@gmail.com>
2023-04-21 11:53:03 +0100

    added menu bar/nav control

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-const navBtn = document.querySelectorAll(".navBtn");
-const mydropDown = document.querySelectorAll(".dropdown-content");
 
-navBtn.forEach( (btn) => {
-    btn.addEventListener("click", (e) => {
-        mydropDown.classList.toggle("dropdown-unordered")
-    })
-});
+
+
+
+
+
+
+
+
+// const navBtn = document.querySelectorAll(".navBtn");
+// const mydropDown = document.querySelectorAll(".dropdown-content");
+
+// navBtn.forEach( (btn) => {
+//     btn.addEventListener("click", (e) => {
+//         mydropDown.classList.toggle("dropdown-unordered")
+//     })
+// });
